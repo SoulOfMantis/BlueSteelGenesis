1c9d5fc9707ce6f87850adc62bca83c1967d566a
str1ker07 <leshchenko.b.v@gmail.com>
2025-11-22 18:40:17 +0300

    getAvailableCells
    Ищем доступные клетки и запоминаем их

--- Blue Steel Genesis/Assets/Scripts/TilemapTest.cs
+++ Blue Steel Genesis/Assets/Scripts/TilemapTest.cs
@@ -1,3 +1,5 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -12,6 +14,33 @@ public class TilemapTest : MonoBehaviour
         transform.position = pos;
     }
 
+    List<Vector3Int> getAvailableCells(int n, Vector3Int start)
+    {
+        Assert.IsTrue(n >= 0);
+        List<Vector3Int> res = new List<Vector3Int>();
+        HashSet<Vector3Int> toAdd = new HashSet<Vector3Int>();
+        res.Add(start);
+        for (int i = 1; i <= n; i++)
+        {
+            foreach (var cell in res )
+            {
+                toAdd.Add(new Vector3Int(cell.x + 1, cell.y));
+                toAdd.Add(new Vector3Int(cell.x -1 , cell.y ));
+                toAdd.Add(new Vector3Int(cell.x, cell.y + 1));
+                toAdd.Add(new Vector3Int(cell.x, cell.y - 1));
+
+            }
+            foreach (var cell in toAdd)
+            {
+                if (!res.Contains(cell))
+                    res.Add(cell);
+            }
+            toAdd.Clear();
+        }
+        return res;
+        
+
+    }
     // Update is called once per frame
     void Update()
     {
