9082061580e08e044cfdd54188f2f10a148d5ba6
Timofey577 <shashkin@sfedu.ru>
2025-11-27 21:12:33 +0300

    Пустая заглушка для scene tracker
    Co-Authored-By: str1ker07 <231431262+str1ker07@users.noreply.github.com>

---
+++ Blue Steel Genesis/Assets/Scripts/SceneTracker.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class SceneTracker : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
