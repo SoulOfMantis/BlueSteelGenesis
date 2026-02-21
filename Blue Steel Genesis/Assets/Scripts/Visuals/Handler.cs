ddde89ce62d6f943322908741f5c2f829d18e325
Astakh1 <ivan2008.astahov@gmail.com>
2026-02-21 16:00:48 +0300

    Create sprite handler base
    Co-Authored-By: Yulia-Petlenko <183706262+Yulia-Petlenko@users.noreply.github.com>

---
+++ Blue Steel Genesis/Assets/Scripts/Visuals/Handler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public abstract class VisualHandler : MonoBehaviour
+{
+
+}
---
+++ Blue Steel Genesis/Assets/Scripts/Visuals/HandlerBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+public abstract class VisualHandlerBase : MonoBehaviour
+{
+    [SerializeField] protected Animator animator;
+    [SerializeField] protected SpriteRenderer spriteRenderer;
+
+    // Инициализирует обработчик спрайтов и анимаций
+    protected virtual void Inititate()
+    {
+        if (animator == null)
+            animator = GetComponent<Animator>();
+
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Задаёт спрайт объекта
+    public virtual void SetSprite(Sprite sprite)
+    {
+        if (spriteRenderer  != null)
+            spriteRenderer.sprite = sprite;
+    }
+
+    // Проиграть заданную анимацию
+    public virtual void PlayAnimation(string animationName)
+    {
+        if (animator  != null)
+            animator.Play(animationName);
+    }
+
+    // Задать триггер
+    public virtual void SetTrigger(string triggerName)
+    {
+        if (animator != null)
+            animator.SetTrigger(triggerName);
+    }
+
+    // Задать значение параметру
+    public virtual void SetBool(string parameterName, bool value)
+    {
+        if (animator != null)
+            animator.SetBool(parameterName, value);
+    }
+}
