d276cc51b7896c6f44de8deb23cd41c17b553188
Коростелев Иван <bushes-7th@mail.ru>
2026-02-22 15:41:35 +0300

    feat: add GameState, GameRun, Expedition

---
+++ Blue Steel Genesis/Assets/Scripts/GameState/Expedition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Expedition
+{
+    public Expedition(Map.BiomeInfo biome)
+    {
+        Biome = biome;
+    }
+
+    public void startNextStage()
+    {
+        ++BiomeStage;
+        Map = global::Map.ExpeditionMap.generate(
+            5, 9,
+            BitConverter.GetBytes(GameState.Run.GlobalSeed),
+            Biome, (uint)BiomeStage,
+            GameState.Run.PlayerLivesCount,
+            Array.Empty<byte>() //TODO: pass actual data
+        );
+        map_progress_ = new(Map);
+    }
+
+    public void displayMap(ExpeditionMapView view)
+    {
+        if (Map == null || map_progress_ == null)
+            throw new InvalidOperationException("Невозможно отобразить карту до начала этапа");
+        if (view != null)
+            view.make(Map, map_progress_);
+    }
+
+    public Map.ExpeditionMap Map { get; private set; } = null;
+    public Map.BiomeInfo Biome { get; private set; }
+
+    private ExpeditionMapProgressInfo map_progress_ = null;
+    public int BiomeStage { get; private set; } = -1;
+}
