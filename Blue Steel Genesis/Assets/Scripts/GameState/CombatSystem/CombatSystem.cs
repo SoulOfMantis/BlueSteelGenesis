b0a8c695920f0f742dbf97a6ebbbf7dd067d07d6
Тимофей Шашкин <shashkin@sfedu.ru>
2026-03-09 20:32:29 +0300

    Добавлен метод победы и распределение наград
    Награда теперь зависит от типа энкаунтера, доработана система элитных боёв
    
    Co-Authored-By: str1ker07 <231431262+str1ker07@users.noreply.github.com>

--- Blue Steel Genesis/Assets/Scripts/GameState/CombatSystem/CombatSystem.cs
+++ Blue Steel Genesis/Assets/Scripts/GameState/CombatSystem/CombatSystem.cs
@@ -4,16 +4,31 @@ using System.Collections.Generic;
 public class CombatSystem
 {
     uint biome_id, stage_id;
+    internal enum EncounterType
+    {
+        Normal,
+        Elite,
+        Boss
+    }
+    EncounterType current_enc;
 
     Random gen;
 
-    const int max_enc_id = 3;
-    const int materials_given = 1;
-    const int money_given = 1;
+    const uint max_enc_id = 3;
+    const uint max_materials_given = 30;
+    const uint max_money_given = 30;
+    const uint min_materials_given = 5;
+    const uint min_money_given = 5;
+
+    const uint normal_reward_modifier = 1;
+    const uint elite_reward_modifier = 3;
+    const uint boss_reward_modifier = 5;
 
 
-    List<int> elite_variation_list = new List<int>() { 1, 2, 3 };
-    const int default_elite_variation = 0;
+
+    List<uint> elite_list = new List<uint>() { 1, 2, 3 };
+    const uint default_elite = 0;
+    const uint elite_variation_count = 3;
 
     public CombatSystem(uint biome, uint stage, int local_seed)
     {
@@ -24,33 +39,50 @@ public class CombatSystem
 
     string NextNormalEncounter()
     {
-        int enc_id = gen.Next(max_enc_id);
+        uint enc_id = (uint)gen.Next((int)max_enc_id);
         return $"b{biome_id}_st{stage_id}_Normal{enc_id}";
     }
 
     string NextEliteEncounter()
     {
-        int elite_variation;
-        if (elite_variation_list.Count != 0)
+        uint elite_id;
+        if (elite_list.Count != 0)
         {
-            int elite_variation_ind = gen.Next(elite_variation_list.Count);
-            elite_variation = elite_variation_list[elite_variation_ind];
-            elite_variation_list.RemoveAt(elite_variation_ind);
+            int elite_ind = gen.Next(elite_list.Count);
+            elite_id = elite_list[elite_ind];
+            elite_list.RemoveAt(elite_ind);
         }
-        else elite_variation = default_elite_variation;
+        else elite_id = default_elite;
 
-        int enc_id = gen.Next(max_enc_id);
-        return $"b{biome_id}_st{stage_id}_Elite{enc_id}_{elite_variation}";
+        uint elite_variation = (uint)gen.Next((int)elite_variation_count);
+        return $"b{biome_id}_st{stage_id}_Elite{elite_id}_{elite_variation}";
     }
 
     public void TriggerNormalEncounter()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(NextNormalEncounter());
+        current_enc = EncounterType.Normal;
     }
 
     public void TriggerEliteEncounter()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(NextEliteEncounter());
+        current_enc = EncounterType.Elite;
+    }
+
+    void GiveReward(uint modifier)
+    {
+        uint materials_given = min_materials_given;
+        uint money_given = min_money_given;
+
+        for (int i = 0; i < modifier; i++)
+        {
+            materials_given += (uint)gen.Next((int)min_materials_given, (int)max_materials_given);
+            money_given += (uint)gen.Next((int)min_money_given, (int)max_money_given);
+        }
+
+        GameState.Run.Expedition.Player.GiveMaterials((int)materials_given);
+        GameState.Run.Expedition.Player.GiveMoney((int)money_given);
     }
 
     public void Defeat()
@@ -60,8 +92,38 @@ public class CombatSystem
 
     public void Victory()
     {
-        GameState.Run.Expedition.Player.GiveMaterials(materials_given);
-        GameState.Run.Expedition.Player.GiveMoney(money_given);
+        switch (current_enc)
+        {
+            case EncounterType.Normal: 
+                VictoryNormal(); 
+                break;
+            case EncounterType.Elite:
+                VictoryElite();
+                break;
+            case EncounterType.Boss:
+                VictoryBoss();
+                break;
+        }
+    }
+
+    void VictoryNormal()
+    {
+        GiveReward(normal_reward_modifier);
         UnityEngine.SceneManagement.SceneManager.LoadScene("ExpeditionMapTest_usingGameState");
+    }
+
+    void VictoryElite()
+    {
+        GiveReward(elite_reward_modifier);
+        // TODO Добавить получение модуля
+        UnityEngine.SceneManagement.SceneManager.LoadScene("ExpeditionMapTest_usingGameState");
+    }
+
+    void VictoryBoss()
+    {
+        GiveReward(boss_reward_modifier);
+        //TODO: Загрузить чёрный рынок
+        GameState.Run.Expedition.startNextStage();
+        // UnityEngine.SceneManagement.SceneManager.LoadScene("ExpeditionMapTest_usingGameState");
     }
 }   
