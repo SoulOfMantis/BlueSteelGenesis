87b7b142db99b2b43b0bdd21f975e1792f033466
Тимофей Шашкин <shashkin@sfedu.ru>
2026-03-09 18:12:58 +0300

    Add a base for elite enemies

--- Blue Steel Genesis/Assets/Scripts/GameState/CombatSystem/CombatSystem.cs
+++ Blue Steel Genesis/Assets/Scripts/GameState/CombatSystem/CombatSystem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 public class CombatSystem
 {
@@ -9,6 +10,10 @@ public class CombatSystem
     const int max_enc_id = 3;
     const int materials_given = 1;
     const int money_given = 1;
+
+
+    List<int> elite_variation_list = new List<int>() { 1, 2, 3 };
+    const int default_elite_variation = 0;
 
     public CombatSystem(uint biome, uint stage, int local_seed)
     {
@@ -23,9 +28,29 @@ public class CombatSystem
         return $"b{biome_id}_st{stage_id}_Normal{enc_id}";
     }
 
+    string NextEliteEncounter()
+    {
+        int elite_variation;
+        if (elite_variation_list.Count != 0)
+        {
+            int elite_variation_ind = gen.Next(elite_variation_list.Count);
+            elite_variation = elite_variation_list[elite_variation_ind];
+            elite_variation_list.RemoveAt(elite_variation_ind);
+        }
+        else elite_variation = default_elite_variation;
+
+        int enc_id = gen.Next(max_enc_id);
+        return $"b{biome_id}_st{stage_id}_Elite{enc_id}_{elite_variation}";
+    }
+
     public void TriggerNormalEncounter()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(NextNormalEncounter());
+    }
+
+    public void TriggerEliteEncounter()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(NextEliteEncounter());
     }
 
     public void Defeat()
@@ -35,8 +60,8 @@ public class CombatSystem
 
     public void Victory()
     {
-        GameState.Run.Player.GiveMaterials(materials_given);
-        GameState.Run.Player.GiveMoney(money_given);
+        GameState.Run.Expedition.Player.GiveMaterials(materials_given);
+        GameState.Run.Expedition.Player.GiveMoney(money_given);
         UnityEngine.SceneManagement.SceneManager.LoadScene("ExpeditionMapTest_usingGameState");
     }
 }   
