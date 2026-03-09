415b125584efac9754ac6f02f2d4d4bd86443bc4
Andrey <timo@sfedu.ru>
2026-03-09 15:08:13 +0300

    Implement scene for testing and first Boss module
    plan to add ability to have required or forbidden traits for drawing modules

---
+++ Blue Steel Genesis/Assets/Scripts/Debug/Debug_ModuleGeneration.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UIElements;
+using System.Linq;
+
+public class Debug_ModuleGeneration : MonoBehaviour
+{
+    [SerializeField] TMP_Text Name;
+    [SerializeField] TMP_Text Description;
+    [SerializeField] TMP_Text Keywords;
+    public void DrawNextCommonModule()
+    {
+        var module = GameState.Run.Expedition.GetNextCommonModule();
+        Name.text = module?.Name;
+        Description.text = module?.Description();
+        Keywords.text = "";
+        foreach (var k in module?.Keywords)
+            Keywords.text += $"{k}; ";
+    }
+    public void DrawNextBossModule()
+    {
+        var module = GameState.Run.Expedition.GetNextBossModule();
+        Name.text = module?.Name;
+        Description.text = module?.Description();
+        Keywords.text = "";
+        foreach (var k in module?.Keywords)
+            Keywords.text += $"{k}; ";
+    }
+}
---
+++ Blue Steel Genesis/Assets/Scripts/Modules/Specific Modules/DefaultBoss_TEST_ONLY.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DefaultBoss_TEST_ONLY : ActiveModule    
+{
+    uint damageDealt = 20;
+    uint damageTaken = 3;
+    bool usedThisBattle = false;
+    public DefaultBoss_TEST_ONLY():base()
+    {
+        AddKeywords("Boss", "Offense", "Self-damage", "Once per battle");
+    }
+    public override async Task Effect(Character user, Vector3Int pos)
+    {
+        await user.strike(pos, (int)damageDealt);
+        await user.damage((int)3);
+        usedThisBattle = true;
+    }
+    public override string Description()
+    {
+        return $"Deal {damageDealt} damage. Take {damageTaken} damage. Can only be used once per battle.";
+    }
+    public override bool CanBeUsed()
+    {
+        return usedThisBattle;
+    }
+
+}
+
