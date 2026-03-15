d3fe5c8fc0e428e2075b2e1b3ba44d88ff8602a7
Коростелев Иван <bushes-7th@mail.ru>
2026-03-15 18:42:06 +0300

    chore: bind up-to-date fight testing scene with map testing scene

--- Blue Steel Genesis/Assets/Scripts/Debug/Debug_MenuButtonsForFightTest.cs
+++ Blue Steel Genesis/Assets/Scripts/Debug/Debug_MenuButtonsForFightTest.cs
@@ -5,4 +5,10 @@ public class Debug_MenuButtonsForFightTest : MonoBehaviour
 {
     public static void returnToMap() =>
         SceneManager.LoadScene("ExpeditionMapTest_usingGameState");
+
+    public static void restartExpedition() {
+        GameState.Run.endExpedition();
+        GameState.Run.startExpedition(1);
+        returnToMap();
+    }
 }
