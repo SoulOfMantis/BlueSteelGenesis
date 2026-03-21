ce0666c4a80a4f613fc17d6a3b768e184b2439c2
Andrey <timo@sfedu.ru>
2026-03-21 19:28:47 +0300

    kinda fixed tooltip delay not working
    Entity tooltip can now be delayed by clicking.

--- Blue Steel Genesis/Assets/Scripts/UI/Tooltips/EntityTooltipTooltipTrigger.cs
+++ Blue Steel Genesis/Assets/Scripts/UI/Tooltips/EntityTooltipTooltipTrigger.cs
@@ -1,21 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class EntityTooltipTooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class EntityTooltipTooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TooltipSystem.Delay(TooltipSystem.TooltipType.entityTooltip);
+    }
     public void OnPointerEnter(PointerEventData eventData)
     {
         TooltipSystem.Delay(TooltipSystem.TooltipType.entityTooltip);
     }
     public void OnPointerExit(PointerEventData eventData)
-    {
-        TooltipSystem.ResumeHiding(TooltipSystem.TooltipType.entityTooltip);
-    }
-    private void OnMouseEnter()
-    {
-        TooltipSystem.Delay(TooltipSystem.TooltipType.entityTooltip);
-    }
-    private void OnMouseExit()
     {
         TooltipSystem.ResumeHiding(TooltipSystem.TooltipType.entityTooltip);
     }
