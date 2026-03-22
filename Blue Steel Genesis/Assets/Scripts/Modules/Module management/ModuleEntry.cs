ce92e36b042a410db8b7dee0c4a6fb680914dc35
Astakh1 <ivan2008.astahov@gmail.com>
2026-03-22 22:05:56 +0300

    create module manager scene

---
+++ Blue Steel Genesis/Assets/Scripts/Modules/Module management/ModuleEntry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+class ModuleEntry : MonoBehaviour
+{
+    public TMP_Text nameText;
+    public Button upButton;
+    public Button downButton;
+
+    private int currentIdx;
+    private ModuleManagemntUI manager;
+
+    public void Setup(GameModule module, int idx, ModuleManagemntUI mgr)
+    {
+        currentIdx = idx;
+        manager = mgr;
+
+        nameText.text = module.Name;
+        
+        upButton.onClick.RemoveAllListeners();
+        downButton.onClick.RemoveAllListeners();
+
+        upButton.onClick.AddListener(() => manager.MoveModuleUp(currentIdx));
+        downButton.onClick.AddListener(() => manager.MoveModuleDown(currentIdx));
+        upButton.interactable = idx > 0;
+        downButton.interactable = idx < ModuleManager.Modules.Count - 1;
+    }
+}
---
+++ Blue Steel Genesis/Assets/Scripts/Modules/Module management/ModuleManagemntUI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+class ModuleManagemntUI : MonoBehaviour
+{
+    public GameObject moduleEntryPrefab;
+    public Transform contentParent;
+    public Button closeButton;
+
+    private List<ModuleEntry> entries = new List<ModuleEntry>();
+
+    private void OnEnable()
+    {
+        ModuleManager.ModulesChanged += RefreshList;
+    }
+
+    void RefreshList()
+    {
+        foreach (var entry in entries)
+            Destroy(entry.gameObject);
+        entries.Clear();
+
+        var modules = ModuleManager.Modules;
+        for (int i = 0; i < modules.Count; i++)
+        {
+            var go = Instantiate(moduleEntryPrefab, contentParent);
+            var entry = go.GetComponent<ModuleEntry>();
+            entry.Setup(modules[i], i, this);
+            entries.Add(entry);
+        }
+    }
+
+    public void MoveModuleUp(int idx) => ModuleManager.MoveModuleUp(idx);
+    public void MoveModuleDown(int idx) => ModuleManager.MoveModuleDown(idx);
+
+    void CloseMenu()
+    {
+        return;
+    }
+}
+
