2d0d20b9c19582d1c29c294a4311174d900456b6
Andrey <timo@sfedu.ru>
2026-03-24 23:49:53 +0300

    treasure room layout finished (more or less)

--- Blue Steel Genesis/Assets/Scripts/GameState/TreasureSystem/testTreasureGeneration.cs
+++ Blue Steel Genesis/Assets/Scripts/GameState/TreasureSystem/testTreasureGeneration.cs
@@ -5,24 +5,25 @@ using UnityEngine.UI;
 public class testTreasureGeneration : MonoBehaviour
 {
     [SerializeField] Button exit;
-    [SerializeField] Button reroll;
-    [SerializeField] TMP_Text moduleName;
+    [SerializeField] Button openTheChest;
+    [SerializeField] ModuleTooltipTrigger trigger;
 
-    void Reroll()
+    void Take()
     {
-        GameState.Run.Expedition.TreasureSubsystem.RerollTreasure();
-        UpdateModuleName();
+        if (ModuleManager.AddModule(GameState.Run.Expedition.TreasureSubsystem.Treasure))
+        {
+            trigger.gameObject.SetActive(false);
+            Destroy(trigger);
+            openTheChest.gameObject.SetActive(false);
+            Destroy(openTheChest);
+        }
     }
     void Exit() => UnityEngine.SceneManagement.SceneManager.LoadScene("ExpeditionMapTest_usingGameState");
     private void Start()
     {
+        GameState.Run.Expedition.TreasureSubsystem.RerollTreasure();
+        trigger.updateModuleTrigger(GameState.Run.Expedition.TreasureSubsystem.Treasure);
         exit.onClick.AddListener(Exit);
-        reroll.onClick.AddListener(Reroll);
-        Reroll();
+        openTheChest.onClick.AddListener(Take);
     }
-    void UpdateModuleName()
-    {
-        moduleName.text = GameState.Run.Expedition.TreasureSubsystem.Treasure.Name;
-    }
-
 }
