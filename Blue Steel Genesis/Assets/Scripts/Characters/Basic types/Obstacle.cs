5872faf99c1056007d39ec34ea4c6ad22d3fe269
Тимофей Шашкин <shashkin@sfedu.ru>
2026-03-27 01:03:47 +0300

    Add bush

--- Blue Steel Genesis/Assets/Scripts/Characters/Basic types/Obstacle.cs
+++ Blue Steel Genesis/Assets/Scripts/Characters/Basic types/Obstacle.cs
@@ -1,7 +1,21 @@
+using System.Threading.Tasks;
 public abstract class Obstacle : Entity {
     public override URangeValue currentHealth { get; protected set; } = new();
     public override uint maxHealth {
         get => currentHealth.Max;
         protected set => currentHealth.Max = value;
+    }
+    public Obstacle(uint maxHealth)
+    {
+        this.maxHealth = maxHealth;
+        currentHealth.Max = maxHealth;
+        currentHealth.Value = maxHealth;
+    }
+
+    protected override Task die()
+    {
+        tracker.RemoveObstacle(this);
+        Destroy(gameObject);
+        return Task.CompletedTask;
     }
 }
