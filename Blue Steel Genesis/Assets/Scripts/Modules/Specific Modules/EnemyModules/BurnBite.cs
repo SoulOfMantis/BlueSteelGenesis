b6d23649e66aa38f3f6769cbfc6a09a8aaa8d06f
Богдан Лещенко <leshchenko.b.v@gmail.com>
2026-03-27 18:29:06 +0300

    Implemented BurnBite

---
+++ Blue Steel Genesis/Assets/Scripts/Modules/Specific Modules/EnemyModules/BurnBite.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class BurnBite : ActiveModule
+{
+    uint hitDamage = 3;
+    uint burnDamage = 1;
+    uint burnDuration = 1;
+    public BurnBite() : base()
+    {
+        range = 1;
+        energyCost = 2;
+        AddConstKeywords(new OffenseKeyword());
+    }
+    public override HashSet<ModuleKeyword> renewableKeywords()
+    {
+        var res = base.renewableKeywords();
+        res.Add(new InflictKeyword<BurnModule>(PossibleTargets.Target, burnDamage, burnDuration));
+        return res;
+    }
+    public override async Task Effect(Character user, Vector3Int pos)
+    {
+        await user.strike(pos, hitDamage);
+        await user.apply(pos, new BurnModule(burnDamage, burnDuration));
+    }
+    public override string Description()
+    {
+        return $"Deals {hitDamage} damage to the adjacent cell.\n" + base.Description();
+    }
+    protected override bool checkFinalPosition(Vector3Int pos)
+    {
+        return Entity.tracker.IsOccupied(pos);
+    }
+    public override bool checkPosition(Character user, Vector3Int pos)
+    {
+        return base.checkPosition(user, pos) && !user.Position.Contains(pos);
+    }
+}
+
