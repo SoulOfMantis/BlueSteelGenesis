002c12ed77781d799aef370af9571bdae86aa6ff
Andrey <timo@sfedu.ru>
2026-03-27 12:15:23 +0300

    buff player's starting stats and poison module

--- Blue Steel Genesis/Assets/Scripts/GameState/Expedition.cs
+++ Blue Steel Genesis/Assets/Scripts/GameState/Expedition.cs
@@ -19,8 +19,8 @@ public class Expedition
             new BasicMovement(),
             new DogSummoner_TEST_ONLY()
         };
-        Player.maxHealth = 10;
-        Player.maxEnergy = 3;
+        Player.maxHealth = 50;
+        Player.maxEnergy = 5;
         Player.GiveMaterials(3);
         Player.GiveMoney(10);
         Player.currentHealth.Value = Player.maxHealth;
