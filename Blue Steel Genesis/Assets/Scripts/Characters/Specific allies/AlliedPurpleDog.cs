58f867ec33043273c9f8b2acc6ad15f20a90a34e
Коростелев Иван <bushes-7th@mail.ru>
2026-04-26 18:44:44 +0300

    refactor(NPC): add default handlers for common movement logic

--- Blue Steel Genesis/Assets/Scripts/Characters/Specific allies/AlliedPurpleDog.cs
+++ Blue Steel Genesis/Assets/Scripts/Characters/Specific allies/AlliedPurpleDog.cs
@@ -25,21 +25,10 @@ public class AlliedPurpleDog : Ally
         targetPos = possibleTargets.FirstOrDefault();
         return possibleTargets.Count() != 0;
     }
-    protected override bool TryGetTargetForOne(out Vector3Int targetPos)
-    {
-        targetPos = Position.LeftBottom;
-        var moveRange = priorityModules[1].getCellsInRange(Position).Concat(Position).ToHashSet();
-        var path = Navigation.Dijkstra.getPath(Position, getEnemies().SelectMany(e => e.Position.NeighborPositions()),
-            p => !tracker.IsOccupied(p) && !tracker.OutOfBounds(p)) ?? new();
+    protected override bool TryGetTargetForOne(out Vector3Int targetPos) =>
+        GetDirectApproachTarget(out targetPos, priorityModules[1], getEnemies()) ||
+        GetApproachTarget(out targetPos, priorityModules[1], getEnemies());
 
-        Vector3Int offset = new();
-        foreach (var move in path)
-            if ((Position + offset + move).All(p => moveRange.Contains(p)))
-                offset += move;
-            else break;
-        targetPos = Position.LeftBottom + offset;
-        return offset != Vector3Int.zero;
-    }
     public override async Task damage(uint dmg)
     {
         Debug.Log($"Собака получила {dmg} урона!");
