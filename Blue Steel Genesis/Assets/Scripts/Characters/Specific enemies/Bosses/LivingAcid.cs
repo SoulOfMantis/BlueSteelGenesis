22d1a2cebdbec90b0ab9093deddfe1cc12ccc32f
Andrey <timo@sfedu.ru>
2026-05-03 13:24:11 +0300

    small LivingAcid redesign

--- Blue Steel Genesis/Assets/Scripts/Characters/Specific enemies/Bosses/LivingAcid.cs
+++ Blue Steel Genesis/Assets/Scripts/Characters/Specific enemies/Bosses/LivingAcid.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class LivingAcid : Enemy
 {
-    public LivingAcid() : base(100, 5, -10) 
+    public LivingAcid() : base(100, 3, -10) 
     {
         Name = "the Living Acid";
         Description = "The biggest slime there ever was. Will bite. Will shoot. Will... EXPLODE!";
@@ -11,8 +11,8 @@ public class LivingAcid : Enemy
     protected override void Init()
     {
         addModule(new AcidBite());
+        addModule(new BasicMovement(2));
         addModule(new AcidShot());
-        addModule(new BasicMovement(1));
         addModule(new ExplodeWithSlime(bodySize - 1));
         base.Init();
     }
@@ -22,14 +22,15 @@ public class LivingAcid : Enemy
         targetPos = possibleTargets.FirstOrDefault();
         return possibleTargets.Count() != 0;
     }
-    protected override bool TryGetTargetForOne(out Vector3Int targetPos)
+    protected override bool TryGetTargetForOne(out Vector3Int targetPos) =>
+        GetDirectApproachTarget(out targetPos, priorityModules[1], getEnemies()) ||
+        GetApproachTarget(out targetPos, priorityModules[1], getEnemies());
+    
+    protected override bool TryGetTargetForTwo(out Vector3Int targetPos)
     {
-        var possibleTargets = priorityModules[1].getCellsInRange(Position).Where(p => getEnemies().SelectMany(e => e.Position).Contains(p));
+        var possibleTargets = priorityModules[2].getCellsInRange(Position).Where(p => getEnemies().SelectMany(e => e.Position).Contains(p));
         targetPos = possibleTargets.FirstOrDefault();
         return possibleTargets.Count() != 0;
     }
-    protected override bool TryGetTargetForTwo(out Vector3Int targetPos) =>
-        GetDirectApproachTarget(out targetPos, priorityModules[2], getEnemies()) ||
-        GetApproachTarget(out targetPos, priorityModules[2], getEnemies());
 }
 
